be2a30af255c25365d8ddd67ef66fd8ba4ddb998
Jakub Raczyński <j.raczynski1409@gmail.com>
2025-02-27 12:45:29 +0100

    Loading bar, hour settings

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let letters = document.querySelector("#letters");
-let clock = document.querySelector("#clock");
+const letters = document.querySelector("#letters");
+const clock = document.querySelector("#clock");
+const startTime = document.querySelector("#start"); 
+const endTime = document.querySelector("#end"); 
+const loadingBar = document.querySelector("#loading-bar"); 
+const loadingValue = document.querySelector("#loading-value"); 
 
 clock.innerHTML = updateTime();
+updateProgressBar();
+
+startTime.addEventListener("change",()=>{
+    updateProgressBar();
+});
+
+endTime.addEventListener("change",()=>{
+    updateProgressBar();
+});
 
 setInterval(()=>{
     clock.innerHTML = updateTime();
+    updateProgressBar();
 },1000);
+
+
 
 setTimeout(() => {
     startLetterCycle();
     setInterval(randomLetters, 30 * 60 * 1000);
 }, getTimeUntilNextHalfHour());
+
 
 function randomLetters(){
     let vowels = "AEIOU";
@@ -31,6 +48,21 @@ function randomLetters(){
     letters.innerHTML = randomLettersArray;
 
     setTimeout(() => { letters.innerHTML = ""; }, 3 * 60 * 1000);
+}
+
+function updateProgressBar(){
+    const now = new Date();
+    let startHour = new Date();
+    let endHour = new Date();
+    startHour.setHours(parseInt(startTime.value[0]+startTime.value[1]),parseInt(startTime.value[3]+startTime.value[4]),0);
+    endHour.setHours(parseInt(endTime.value[0]+endTime.value[1]),parseInt(endTime.value[3]+endTime.value[4]),0);
+
+    let timeInMinutes = (endHour-startHour)/60000;
+    let currentMinutes = (now-startHour)/60000;
+    let percent = Math.max(Math.min((100*(currentMinutes/timeInMinutes)).toFixed(2),100),0);
+
+    loadingBar.style.width = percent+'%';
+    loadingValue.innerHTML = percent+'%';
 }
 
 function updateTime(){
@@ -57,4 +89,5 @@ function getTimeUntilNextHalfHour() {
 function startLetterCycle() {
     randomLetters();
     setInterval(randomLetters, 30 * 60 * 1000);
+    
 }
